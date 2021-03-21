4efc8491b22e2a1f643e957e9c3b1813cf705337
root <root@DESKTOP-CF0Q36A.localdomain>
2021-03-21 01:25:03 -0300

    Finalizada aula 10

---
+++ lib/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: GestureDetector(child: Text('Contador: $counter')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
